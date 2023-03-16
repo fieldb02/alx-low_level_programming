a585313aa3a71a56cf557c9fd341fe45c4001817
fieldb02 <bonyango118@gmail.com>
2023-03-16 21:35:43 +0000

    betty reminder

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main()
 {
 	char str[] = "Programming is like building a multilingual puzzle";
 	puts(str);
-	return 0;
+	return (0);
 }
