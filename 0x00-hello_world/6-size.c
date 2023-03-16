555943b5fd269cc313f3e74009628e7bc4777353
Billy Onyango <bonyango118@gmail.com>
2023-03-16 16:23:47 -0700

    correct

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,6 @@
  */
 int main(void)
 {
-	char charType;
-	int intType;
-	long longType;
-	long long longlongType;
-	float floatType;
-
 	printf("size of a char: %d byte(s)\n", sizeof(char));
 	printf("size of an int: %d byte(s)\n", sizeof(int));
 	printf("size of a long: %d byte(s)\n", sizeof(long));
