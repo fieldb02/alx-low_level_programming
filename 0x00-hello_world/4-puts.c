03d3b4280c71ffbe9edfa8b1012e0235aeb8bb64
fieldb02 <bonyango118@gmail.com>
2023-03-16 21:20:25 +0000

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	/*initializing string*/
+
+	char str[] = "Programming is like building a multilingual puzzle";
+	
+	/*writing stdout*/
+
+	puts(str);
+
+	return 0;
+}
