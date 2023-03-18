111dfacba0fd2e68d3502a5b9cbde1efd1f8d603
fieldb02 <bonyango118@gmail.com>
2023-03-18 00:49:15 +0000

    Numbers in base 10

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * main - Prints all letters except q and e
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char alphabet;
+
+	alphabet = 'a'
+	if (alphabet == 'e' & 'q')
+	{
+		putchar(alphabet);
+		alphabet++;
+	}
+	putchar('\n');
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+	{
+	printf("%d", x);
+	}
+	if (x < 9)
+	{
+	printf(", ");
+	}
+	printf("\n");
+	return (0);
+}
