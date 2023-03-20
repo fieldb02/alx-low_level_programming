e60d56de8da9d9d0d0215ce4890a8e724948b4c2
fieldb02 <bonyango118@gmail.com>
2023-03-20 01:26:26 +0000

    Pulled. Now, push

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,19 +9,25 @@
  */
 int main(void)
 {
-   int i, j, k;
+	int i, j, k;
 
-   for (i = 0; i < 10; i++) {
-      for (j = 0; j < 10; j++) {
-         for (k = 0; k < 10; k++) {
-            if (i != j && j != k && i != k) {
-               putchar(i + '0');
-               putchar(j + '0');
-               putchar(k + '0');
-            }
-         }
-      }
-   }
-	putchar('0');
-   return (0);
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (k = 0; k < 10; k++)
+			{
+				if (i != j && j != k && i != k)
+				{
+					putchar(i + '0');
+					putchar(j + '0');
+					putchar(k + '0');
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
