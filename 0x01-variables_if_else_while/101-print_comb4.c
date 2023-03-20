85d75a26cdfd13774a6bce1e6cb8dc6f1de86efb
Billy Onyango <bonyango118@gmail.com>
2023-03-19 18:38:40 -0700

    Almost there

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,11 +11,11 @@ int main(void)
 {
 	int i, j, k;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; j < 9; j++)
 		{
-			for (k = 0; k < 10; k++)
+			for (k = 0; k < 9; k++)
 			{
 				if (i != j && j != k && i != k)
 				{
