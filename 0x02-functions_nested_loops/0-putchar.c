aa368cfc7f91734a50c7368967fdb7873aa9d9df
Billy Onyango <bonyango118@gmail.com>
2023-03-21 19:04:42 +0300

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+/**
+ *  main - Print _putchar
+ *
+ *  Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
