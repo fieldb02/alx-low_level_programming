8dceeef7b4251e7b200e8a3fab83d42be1934150
Billy Onyango <bonyango118@gmail.com>
2023-03-22 08:03:47 +0300

    Success

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -4,10 +4,15 @@
 /**
  * add - adds two integers and returns the result
  *
- * Return: 0
+ * @i: integer to be added
+ *
+ * @j: integer to be added
+ *
+ * Return: the result
  */
 int add(int i, int j)
 {
 	int x = i + j;
+
 	return (x);
 }
