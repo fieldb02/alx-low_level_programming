05abb8bd1dea6241981f8b1dd1004d54908a3c0c
Billy Onyango <bonyango118@gmail.com>
2023-03-22 05:56:12 +0300

    Success

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,52 @@
+#include "main.h"
+/**
+ * times_table - Prints the 9 times tables
+ *
+ * Return: 0
+ */
+
+void times_table(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			int x = i * j;
+			if (x >= 9)
+			{
+				_putchar(x / 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+				_putchar(',');
+			}
+			_putchar(x % 10 + '0');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
