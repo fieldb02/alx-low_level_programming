7e8cacdce3a45d3df8c8e033b2d746fb0ab1517c
Billy Onyango <bonyango118@gmail.com>
2023-03-23 01:00:07 +0300

    Success

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,27 +9,27 @@ void times_table(void)
 	int i;
 	int j;
 
-	for (i = 0; i <= 9; i++)
+		for (i = 0; i <= 9; i++)
 	{
 		for (j = 0; j <= 9; j++)
-	{
-		int x = i * j;
+		{
+			int x = i * j;
 
-		if (x > 9)
-	{
-		_putchar(x / 10 + '0');
-	}
-	else
-	{
-		_putchar(' ');
-	}
-		_putchar(x % 10 + '0');
-	if (j != 9)
-	{
-		_putchar(',');
-	}
-		_putchar(' ');
-	}
+			if (x > 9)
+			{
+				_putchar(x / 10 + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar(x % 10 + '0');
+			if (j != 9)
+			{
+				_putchar(',');
+			}
+			_putchar(' ');
+		}
 		_putchar('\n');
 	}
 }
