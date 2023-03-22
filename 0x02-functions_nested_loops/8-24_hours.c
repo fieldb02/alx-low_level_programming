6ba5cd787d89e80ec932a47a0c10776f517e789f
Billy Onyango <bonyango118@gmail.com>
2023-03-22 03:43:22 +0300

    Success

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * jack_bauer - Prints every minute of the day for jack
+ *
+ * Return: void
+ */
+
+void jack_bauer(void)
+{
+	int hour;
+	int minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			putchar(hour / 10 + '0');
+			putchar(hour % 10 + '0');
+			putchar(':');
+			putchar(minute / 10 + '0');
+			putchar(minute % 10 + '0');
+			putchar('\n');
+		}
+	}
+}
+
