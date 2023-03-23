5492939b018d6ebde3824782d7ead8d91112766e
Billy Onyango <bonyango118@gmail.com>
2023-03-23 11:04:24 +0300

    Success 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_numbers - prints numbers
+ *
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 48; a <= 57; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
