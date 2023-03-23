b246c73fb70480f7af6e729290abc90df7e8916f
Billy Onyango <bonyango118@gmail.com>
2023-03-23 11:46:30 +0300

    Success 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - Prints numbers in multiple
+ *
+ * Return: 0
+ */
+void  more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 14; b++)
+		{
+			if (b > 9)
+			{
+				_putchar(b / 10 + '0');
+			}
+			_putchar(b % 10 + '0');
+		}
+		_putchar(10);
+	}
+}
