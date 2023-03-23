cf39e9fd76417c2b903319edda6f5192a857ee3d
Billy Onyango <bonyango118@gmail.com>
2023-03-23 13:12:20 +0300

    Success 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * print_square - Prints squares
+ *
+ * @size: Size of the square
+ *
+ * Return: 0
+ */
+void print_square(int size)
+{
+	int a;
+	int b;
+
+	if (size <= 0)
+	{
+		_putchar(10);
+	}
+	else
+	{
+		for (a = 0; a < size; a++)
+		{
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+	}
+}
