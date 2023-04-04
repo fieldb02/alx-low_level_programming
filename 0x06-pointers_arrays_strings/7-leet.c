67898ea10644709c77086a81248eea4ccc16c1bf
Billy Onyango <bonyango118@gmail.com>
2023-04-04 08:27:43 +0300

    Success

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -3,27 +3,27 @@
 #include <stdio.h>
 
 /**
- * leet - Encodes string to an integer
+ * *leet - Encodes string to an integer
  * @str: String to Encode
- * Retuen: Encoded string
+ * Return: Encoded string
  */
 char *leet(char *str)
 {
-    char leet_chars[] = "AaEeOoTtLl";
-    char leet_nums[] = "4433007711";
-    int i;
-    size_t j;
+	char leet_chars[] = "AaEeOoTtLl";
+	char leet_nums[] = "4433007711";
+	int i;
+	size_t j;
 
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        for (j = 0; j < strlen(leet_chars); j++)
-        {
-            if (str[i] == leet_chars[j])
-            {
-                str[i] = leet_nums[j];
-                break;
-            }
-        }
-    }
-    return str;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < strlen(leet_chars); j++)
+		{
+			if (str[i] == leet_chars[j])
+			{
+				str[i] = leet_nums[j];
+				break;
+			}
+		}
+	}
+	return (str);
 }
