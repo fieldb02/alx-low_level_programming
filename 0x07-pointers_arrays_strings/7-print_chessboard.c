dbdf416a217636cb29dab617ec84ea5bbc58813b
Billy Onyango <bonyango118@gmail.com>
2023-04-07 13:56:41 +0300

    Success

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -9,34 +9,15 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	char c;
 	int i;
 	int j;
 
-	_putchar(32);
-	for (c = 'A'; c <= 'H'; c++)
-	{
-		_putchar(32);
-		_putchar(c);
-	}
-	_putchar(32);
 	for (i = 0; i < 8; i++)
 	{
-		_putchar('1' + i);
-		_putchar(32);
 		for (j = 0; j < 8; j++)
 		{
 			_putchar(a[i][j]);
-			_putchar(32);
 		}
-		_putchar('1' + i);
 		_putchar(10);
 	}
-	_putchar(32);
-	for (c = 'A'; c <= 'H'; c++)
-	{
-		_putchar(32);
-		_putchar(c);
-	}
-	_putchar(10);
 }
