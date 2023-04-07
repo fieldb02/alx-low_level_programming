3947e35eb83c2184203b8bd3d53d38e323befd3b
Billy Onyango <bonyango118@gmail.com>
2023-04-07 19:52:30 +0300

    Error fixed

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -3,6 +3,30 @@
 #include <string.h>
 
 /**
+ * is_prime_helper - Checks if the number is less than or equal
+ * to 2 and returns 2, else, 0
+ * @n: 1st integer
+ * @i: 2nd integer
+ *
+ * Return: 2, else 0
+ */
+int is_prime_helper(int n, int i)
+{
+	if (n <= 2)
+	{
+		return ((n == 2) ? 1 : 0);
+	}
+	if (n % i == 0)
+	{
+		return (0);
+	}
+	if (i * i > n)
+	{
+		return (1);
+	}
+	return (is_prime_helper(n, i + 1));
+}
+/**
  * is_prime_number - Returns 1 if the input is a prime integer
  * otherwise return 0
  * @n: Integer used
@@ -10,29 +34,9 @@
  */
 int is_prime_number(int n)
 {
-	int i;
-
 	if (n <= 1)
 	{
 		return (0);
 	}
-	if (n <= 3)
-	{
-		return (1);
-	}
-	if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0);
-	}
-	i = 5;
-	while (i * i <= n)
-	{
-		if (n % i == 0 || n % (i + 2) == 0)
-		{
-			return (0);
-		}
-		i += 6;
-	}
-	return (1);
+	return (is_prime_helper(n, 2));
 }
-
