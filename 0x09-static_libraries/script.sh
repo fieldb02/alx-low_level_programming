1bdd17dee8b6f4d12be2ff3a40cbb89e1398f6b9
fieldb02 <bonyango118@gmail.com>
2023-04-12 19:22:31 +0300

    Success

--- 0x09-static_libraries/script.sh
+++ 0x09-static_libraries/script.sh
@@ -42,4 +42,6 @@ gcc -c 6-abs.c -o 6-abs.o
 gcc -c 9-strcpy.c -o 9-strcpy.o
 gcc -c _putchar.c -o _putchar.o
 
-ar rc libmy.a 0-isupper.o 0-memset.o 0-strcat.o 1-isdigit.o 1-memcpy.o 1-strncat.o 100-atoi.o 2-strchr.o 2-strlen.o 2-strncpy.o 3-islower.o 3-puts.o 3-strcmp.o 3-strspn.o 4-isalpha.o 4-strpbrk.o 5-strstr.o 6-abs.o 9-strcpy.o _putchar.o
+ar rcs libmy.a 0-isupper.o 0-memset.o 0-strcat.o 1-isdigit.o 1-memcpy.o 1-strncat.o 100-atoi.o 2-strchr.o 2-strlen.o 2-strncpy.o 3-islower.o 3-puts.o 3-strcmp.o 3-strspn.o 4-isalpha.o 4-strpbrk.o 5-strstr.o 6-abs.o 9-strcpy.o _putchar.o
+
+
