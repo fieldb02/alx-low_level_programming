28175acc300c5fe9954b3d0af07fc928a6bd0ce9
fieldb02 <bonyango118@gmail.com>
2023-04-12 20:07:25 +0300

    Success

--- 0x09-static_libraries/script.sh
+++ 0x09-static_libraries/script.sh
@@ -21,27 +21,25 @@ echo "unsigned int _strspn(char *s, char *accept);" > 3-strspn.c
 echo "char *_strpbrk(char *s, char *accept);" > 4-strpbrk.c
 echo "char *_strstr(char *haystack, char *needle);" > 5-strstr.c
 
-gcc -c 0-isupper.c -o 0-isupper.o
-gcc -c 0-memset.c -o 0-memset.o
-gcc -c 0-strcat.c -o 0-strcat.o
-gcc -c 1-isdigit.c -o 1-isdigit.o
-gcc -c 1-memcpy.c -o 1-memcpy.o
-gcc -c 1-strncat.c -o 1-strncat.o
-gcc -c 100-atoi.c -o 100-atoi.o
-gcc -c 2-strchr.c -o 2-strchr.o
-gcc -c 2-strlen.c -o 2-strlen.o
-gcc -c 2-strncpy.c -o 2-strncpy.o
-gcc -c 3-islower.c -o 3-islower.o
-gcc -c 3-puts.c -o 3-puts.o
-gcc -c 3-strcmp.c -o 3-strcmp.o
-gcc -c 3-strspn.c -o 3-strspn.o
-gcc -c 4-isalpha.c -o 4-isalpha.o
-gcc -c 4-strpbrk.c -o 4-strpbrk.o
-gcc -c 5-strstr.c -o 5-strstr.o
-gcc -c 6-abs.c -o 6-abs.o
-gcc -c 9-strcpy.c -o 9-strcpy.o
-gcc -c _putchar.c -o _putchar.o
+gcc -c 0-isupper.c
+gcc -c 0-memset.c
+gcc -c 0-strcat.c
+gcc -c 1-isdigit.c
+gcc -c 1-memcpy.c
+gcc -c 1-strncat.c
+gcc -c 100-atoi.c
+gcc -c 2-strchr.c
+gcc -c 2-strlen.c
+gcc -c 2-strncpy.c
+gcc -c 3-islower.c
+gcc -c 3-puts.c
+gcc -c 3-strcmp.c
+gcc -c 3-strspn.c
+gcc -c 4-isalpha.c
+gcc -c 4-strpbrk.c
+gcc -c 5-strstr.c
+gcc -c 6-abs.c
+gcc -c 9-strcpy.c
+gcc -c _putchar.c
 
-ar rcs libmy.a 0-isupper.o 0-memset.o 0-strcat.o 1-isdigit.o 1-memcpy.o 1-strncat.o 100-atoi.o 2-strchr.o 2-strlen.o 2-strncpy.o 3-islower.o 3-puts.o 3-strcmp.o 3-strspn.o 4-isalpha.o 4-strpbrk.o 5-strstr.o 6-abs.o 9-strcpy.o _putchar.o
-
-
+ar rcs libmy.a *.o
