02f2898d12591183e6309a6b8e379b2d284e038c
fieldb02 <bonyango118@gmail.com>
2023-04-17 14:08:38 +0300

    Success

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,4 +1,6 @@
-#include <stdio.h>
+#ifndef DOG_H
+#define DOG_H
+
 /**
  * struct dog - structure info of a dog
  *
@@ -12,3 +14,9 @@ struct dog
 	char *owner;
 	float age;
 };
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
