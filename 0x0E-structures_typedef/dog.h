463fa80d09abc0908789716d74cda2084201a325
fieldb02 <bonyango118@gmail.com>
2023-04-17 15:15:29 +0300

    Success

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,5 @@
 #ifndef DOG_H
 #define DOG_H
-
 /**
  * struct dog - structure info of a dog
  *
@@ -14,9 +13,14 @@ struct dog
 	char *owner;
 	float age;
 };
+
+/**
+ * dog_t - Typedef for dog structur
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif
