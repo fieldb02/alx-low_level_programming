9f31814ddb5b9f060e6ff7ad7317fd3aadc18136
fieldb02 <bonyango118@gmail.com>
2023-04-20 03:12:26 +0300

    Done

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -16,15 +16,18 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (array && cmp && size)
 	{
-		return (-1); /*If size is 0 or negative*/
-	}
-	for (i = 0; i < size; i++)
-	{
-		if (cmp(array[i]) != 0)
+		if (size <= 0)
 		{
-			return (i);
+			return (-1); /*If size is 0 or negative*/
+		}
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
 	return (-1); /*If no element matches*/
