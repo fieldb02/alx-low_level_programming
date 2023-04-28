d6438c5894cd23dd164e96472ff38681058b2b0c
fieldb02 <bonyango118@gmail.com>
2023-04-28 17:53:13 +0300

    Task 0, done

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,23 @@
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * print_list - prints all the elements of a list_t
+ * @h: pointer to the start of the list
+ *
+ * Return: Number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		printf("[%d] %s\n", h->len, (h->str != NULL ? h->str : "(nill)"));
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
