aa36cca3fc01b510964895861d7460c2f815fe1c
fieldb02 <bonyango118@gmail.com>
2023-04-28 21:23:35 +0300

    Done

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,17 +1,32 @@
 section .data
-    msg db "Hello, Holberton", 10
-	len equ $-msg
+    hello db "Hello, Holberton", 10   ; Message to print
+    len equ $-hello                 ; Length of the message
 
 section .text
     global main
 
 main:
-    mov edx, len
-    mov ecx, msg
-    mov ebx, 1 
-    mov eax, 4
-    int 0x80
+    ; Open standard output file descriptor
+    mov rax, 0x02     ; System call for open()
+    mov rdi, 0x01     ; File descriptor for stdout
+    mov rsi, 0x02     ; Open for writing
+    xor rdx, rdx      ; File mode
+    syscall
 
-    mov eax, 1
-    xor ebx, ebx
-    int 0x80
+    ; Write message to stdout
+    mov rax, 0x01     ; System call for write()
+    mov rdi, rax      ; Use the return value from open() as file descriptor
+    mov rsi, hello    ; Pointer to the message
+    mov rdx, len      ; Message length
+    syscall
+
+    ; Close file descriptor for stdout
+    mov rax, 0x03     ; System call for close()
+    mov rdi, 0x01     ; File descriptor for stdout
+    syscall
+
+    ; Exit
+    mov rax, 0x3C     ; System call for exit()
+    xor rdi, rdi      ; Return code 0
+    syscall
+
