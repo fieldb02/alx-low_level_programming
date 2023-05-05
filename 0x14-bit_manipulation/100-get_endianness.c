20cb01b9c6bf71fbd4cf5a3e9c25d7aa9c832472
fieldb02 <bonyango118@gmail.com>
2023-05-05 03:43:42 +0300

    Success?

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_endianness - Checks the endianness of the machine
+ * Return: 0, else, 1
+ */
+int get_endianness(void)
+{
+	int i = 1;
+	char *c = (char *)&i;
+
+	return (*c);
+}
